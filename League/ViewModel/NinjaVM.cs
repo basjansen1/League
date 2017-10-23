a42c10ff2b87197bd20065046838b7ecca3212c3
Bas <basjansen14@outlook.com>
2017-10-23 19:37:34 +0200

    Update NinjaVM.cs

--- League/ViewModel/NinjaVM.cs
+++ League/ViewModel/NinjaVM.cs
@@ -1,12 +1,51 @@
-﻿using System;
+using GalaSoft.MvvmLight;
+using League.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace League.ViewModel
 {
-    public class NinjaVM
+    public class NinjaVM : ViewModelBase
     {
+        private Ninja _ninja;
+
+        public NinjaVM()
+        {
+            _ninja = new Ninja();
+        }
+
+        public NinjaVM(Ninja ninja)
+        {
+            _ninja = ninja;
+        }
+
+        public int Id
+        {
+            get { return _ninja.Id; }
+            set
+            {
+                _ninja.Id = value;
+                base.RaisePropertyChanged("Id");
+            }
+        }
+
+        public int AmountOfGold
+        {
+            get { return _ninja.AmountGold; }
+            set
+            {
+                _ninja.AmountGold = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        public Ninja ToModel()
+        {
+            return _ninja;
+        }
     }
 }
