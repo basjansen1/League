33271d86dc193bfa8f411ae419c229388fe46e87
ArjunAutar01 <asautar@avans.nl>
2017-10-23 15:22:43 +0200

    created bases

--- League/ViewModel/CategoryListVM.cs
+++ League/ViewModel/CategoryListVM.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace League.ViewModel
 {
-    class CategoryListVM
+    public class CategoryListVM : CrudObject<CategoryVM>
     {
+        public override void AddItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EditItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowAddWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowEditWindow()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
