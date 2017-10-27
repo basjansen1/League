c388446e15598eac622cdac0e0f759e1c4340fcb
= <basjansen14@outlook.com>
2017-10-27 15:24:39 +0200

    fully implemented adding a ninja

--- League/ViewModel/AddNinjaVM.cs
+++ League/ViewModel/AddNinjaVM.cs
@@ -2,36 +2,58 @@
 using League.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace League.ViewModel
 {
     public class AddNinjaVM : AddVM<NinjaListVM, NinjaVM>
     {
+        public ICommand AddNinjaCommand { get; set; }
+        public string NameText { get; set; }
+        public string AmountGoldText { get; set; }
+
         public AddNinjaVM(NinjaListVM ViewModelList)
             : base(ViewModelList)
         {
             NewItem = new NinjaVM();
             VMList = ViewModelList;
 
-            AddCommand = new RelayCommand(AddItem);
-            MessageBox.Show("NASI");
+            AddNinjaCommand = new RelayCommand(AddItem);
         }
 
         public override void AddItem()
         {
-            if (!CanAdd())
-                return;
-
             using (var context = new LeagueNinjasDBEntities())
             {
-                NewItem.Id = context.Equipments.Max(i => i.Id) + 1; // Get the highest ID and increment this
+                NewItem.Id = context.Ninjas.Max(i => i.Id) + 1; // Get the highest ID and increment this
+                NewItem.Name = NameText;
+                // TODO CHECK IF PARSING WENT RIGHT
+                NewItem.AmountOfGold = Int32.Parse(AmountGoldText);
+
+                if (!CanAdd())
+                    return;
+
                 VMList.ItemList.Add(NewItem);
                 context.Ninjas.Add(NewItem.ToModel());
-                context.SaveChanges();
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in entityValidationErrors.ValidationErrors)
+                        {
+                            Console.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                        }
+                    }
+                }
             }
             VMList.HideAddWindow();
         }
