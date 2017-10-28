420124de90bd2fe9f5383e419b114860c3c8c8e5
ArjunAutar01 <asautar@avans.nl>
2017-10-28 20:03:13 +0200

    implemented InventoryVM and related commands

---
+++ League/Utils/SellAllItemsCommand.cs
@@ -0,0 +1,45 @@
+﻿using League.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace League.ViewModel
+{
+    public class SellAllItemsCommand : ICommand
+    {
+        private InventoryVM _inventoryVM;
+
+        public SellAllItemsCommand(InventoryVM inventoryVM)
+        {
+            _inventoryVM = inventoryVM;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            using (var context = new LeagueNinjasDBEntities())
+            {
+               return context.Ninjas.Where(n => n.Equals(_inventoryVM.SelectedNinja.ToModel())).First().Equipments.Count != 0;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            using (var context = new LeagueNinjasDBEntities())
+            {
+                var ninjaEquipments = context.Ninjas.Where(n => n.Equals(_inventoryVM.SelectedNinja.ToModel())).First().Equipments.ToList();
+
+                foreach(Equipment e in ninjaEquipments)
+                {
+                    ninjaEquipments.Remove(e);
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
