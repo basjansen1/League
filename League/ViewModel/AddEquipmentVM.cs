ad5827de76d344eed838bd1a468a92a9fc606843
ArjunAutar01 <asautar@avans.nl>
2017-10-28 14:36:04 +0200

    supplement AddCategory and AddEquipment

--- League/ViewModel/AddEquipmentVM.cs
+++ League/ViewModel/AddEquipmentVM.cs
@@ -12,6 +12,7 @@ namespace League.ViewModel
 {
     public class AddEquipmentVM : AddVM<EquipmentListVM, EquipmentVM>
     {
+        public string NewCategoryName { get; set; }
         public AddEquipmentVM(EquipmentListVM ViewModelList) 
             : base(ViewModelList)
         {
@@ -20,16 +21,19 @@ namespace League.ViewModel
         }
         public override void AddItem()
         {
-            using (var context = new LeagueNinjasDBEntities())
+            if (CanAdd())
             {
-                NewItem.Id = context.Equipments.Max(i => i.Id) + 1; // Get the highest ID and increment this
+                using (var context = new LeagueNinjasDBEntities())
+                {
+                    NewItem.Id = context.Equipments.Max(i => i.Id) + 1; // Get the highest ID and increment this
 
-                VMList.ItemList.Add(NewItem);
-                context.Equipments.Add(NewItem.ToModel());
+                    VMList.ItemList.Add(NewItem);
+                    context.Equipments.Add(NewItem.ToModel());
 
-                context.SaveChanges();
+                    context.SaveChanges();
+                }
+                VMList.HideAddWindow();
             }
-            VMList.HideAddWindow();
         }
 
         public override bool CanAdd()
@@ -38,6 +42,33 @@ namespace League.ViewModel
             {
                 MessageBox.Show("You have to give a name to the equipment!");
                 return false;
+            } else if (NewItem.Strength == 0)
+            {
+                MessageBox.Show("You have to assign a strenght value higher than 0");
+                return false;
+            } else if (NewItem.Intelligence == 0)
+            {
+                MessageBox.Show("You have to assign an intelligence value higher than 0");
+                return false;
+            } else if (NewItem.Agility == 0)
+            {
+                MessageBox.Show("You have to assign an agility value higher than 0");
+                return false;
+            } else if (NewItem.Price == 0)
+            {
+                MessageBox.Show("You have to assign a price value higher than 0");
+                return false;
+            } else if (NewCategoryName != null || NewItem.SetCategory(NewCategoryName))
+            {
+                if(NewCategoryName != null)
+                {
+                    MessageBox.Show("You have to assign a category");
+                    return false;
+                } else
+                {
+                    MessageBox.Show("This category does not exists");
+                    return false;
+                }
             }
             else
                 return true;
--- League/ViewModel/EquipmentVM.cs
+++ League/ViewModel/EquipmentVM.cs
@@ -116,7 +116,6 @@ namespace League.ViewModel
             {
                 if (context.Categories.Contains(category))
                 {
-                 //   _equipment.
                     return true;
                 } else
                 {
