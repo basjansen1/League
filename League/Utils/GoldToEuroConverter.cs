44fcbeb51762575454832398433b3c3c75c41ba4
ArjunAutar01 <asautar@avans.nl>
2018-01-04 22:37:58 +0100

    added valueConverter

---
+++ League/Utils/GoldToEuroConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace League.Utils
+{
+    public class GoldToEuroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int goldWorth = (int) value;
+            return (int) (goldWorth * 276);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int goldWorth = (int) value;
+            return (int) (goldWorth / 276);
+        }
+    }
+}
